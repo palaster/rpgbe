068d04a9462bfbaff37ce264f4caad2057b120e2
Pat Laster <palaster307@gmail.com>
2022-04-09 23:10:21 -0500

    Finished working on audio

--- src/gameboy/spu.rs
+++ src/gameboy/spu.rs
@@ -8,10 +8,12 @@ const WAVE_FORM: [[u8; 8]; 4] = [
     [0, 1, 1, 1, 1, 1, 1, 0,],
 ];
 
+const SOUND_CHANNEL_4_DIVISOR: [u8; 8] = [8, 16, 32, 48, 64, 80, 96, 112];
+
 pub(crate) trait SoundChannel {
-    fn reset(&mut self, memory: &Memory, length: u8) {}
-    fn update(&mut self, memory: &mut Memory) {}
-    fn get_amplitude(&mut self, memory: &Memory) -> f32 { 0.0 }
+    fn reset(&mut self, memory: &Memory, length: u8);
+    fn update(&mut self, memory: &mut Memory);
+    fn get_amplitude(&mut self, memory: &Memory) -> f32;
 }
 
 pub(crate) struct Spu {
@@ -103,6 +105,7 @@ impl SoundChannel for SoundChannel1 {
 
     fn update(&mut self, memory: &mut Memory) {
         let nr10 = memory.read_from_memory(0xff10);
+        let nr11 = memory.read_from_memory(0xff11);
         let nr12 = memory.read_from_memory(0xff12);
         let nr13 = memory.read_from_memory(0xff13);
         let nr14 = memory.read_from_memory(0xff14);
@@ -119,21 +122,19 @@ impl SoundChannel for SoundChannel1 {
             }
         }
 
-        if (self.frame_sequence == 2 || self.frame_sequence == 6) && self.frame_sequence_timer == 8192 && ((nr10 >> 4) & 7) != 0 && (nr10 & 7) != 0 {
+        if (self.frame_sequence == 2 || self.frame_sequence == 6) && self.frame_sequence_timer == 8192 && ((nr10 >> 4) & 0b111) != 0 && (nr10 & 0b111) != 0 {
             self.sweep_period -= 1;
             if self.sweep_period <= 0 {
-                self.sweep_period = (nr10 >> 4) & 7;
-                if self.sweep_period == 0 {
-                    self.sweep_period = 8;
-                }
-                if ((nr10 >> 4) & 7) != 0 && self.sweep_enabled && (nr10 & 7) != 0 {
-                    let sweep_shift = nr10 & 7;
+                self.sweep_period = (nr10 >> 4) & 0b111;
+                if self.sweep_period == 0 { self.sweep_period = 8; }
+                if ((nr10 >> 4) & 0b111) != 0 && self.sweep_enabled && (nr10 & 0b111) != 0 {
+                    let sweep_shift = nr10 & 0b111;
                     let sweep_negate = if bit_logic::check_bit(nr10, 3) { -1 } else { 1 };
                     let new_frequency = self.sweep_shadow + (self.sweep_shadow >> sweep_shift) * sweep_negate;
                     if new_frequency < 2048 && sweep_shift != 0 {
                         self.sweep_shadow = new_frequency;
                         memory.write_to_memory(0xff13, (self.sweep_shadow & 0xff) as u8);
-                        memory.write_to_memory(0xff14, ((self.sweep_shadow >> 8) & 7) as u8);
+                        memory.write_to_memory(0xff14, ((self.sweep_shadow >> 8) & 0b111) as u8);
                         if self.sweep_shadow + (self.sweep_shadow >> sweep_shift) * sweep_negate > 2047 {
                             self.enabled = false;
                             self.sweep_enabled = false;
@@ -157,20 +158,20 @@ impl SoundChannel for SoundChannel1 {
             self.wave_duty_position += 1;
             self.wave_duty_position %= 8;
         } else {
-            self.frequency_timer = self.frame_sequence_timer.wrapping_sub(1);
+            self.frequency_timer = self.frequency_timer.wrapping_sub(1);
         }
 
-        let duty = memory.read_from_memory(0xff11) >> 6;
+        let duty = nr11 >> 6;
         if WAVE_FORM[duty as usize][self.wave_duty_position as usize] == 1 {
             self.frequency = self.amplitude;
         } else {
             self.frequency = 0;
         }
         
-        if self.frame_sequence == 7 && self.frame_sequence_timer == 8192 && self.envelope_enabled && (nr12 & 7) != 0 {
+        if self.frame_sequence == 7 && self.frame_sequence_timer == 8192 && self.envelope_enabled && (nr12 & 0b111) != 0 {
             self.envelope_sweeps = self.envelope_sweeps.wrapping_sub(1);
             if self.envelope_sweeps == 0 {
-                self.envelope_sweeps = nr12 & 7;
+                self.envelope_sweeps = nr12 & 0b111;
                 let new_amplitude = self.amplitude + if bit_logic::check_bit(nr12, 3) { 1 } else { -1 };
                 if new_amplitude >= 0 && new_amplitude <= 15 {
                     self.amplitude = new_amplitude;
@@ -182,9 +183,8 @@ impl SoundChannel for SoundChannel1 {
         }
     }
 
-    fn get_amplitude(&mut self, memory: &Memory) -> f32 {
-        let nr52: u8 = memory.read_from_memory(0xff26);
-        if self.enabled/* && bit_logic::check_bit(nr52, 2)*/ {
+    fn get_amplitude(&mut self, _memory: &Memory) -> f32 {
+        if self.enabled {
             self.frequency as f32 / 100.0
         } else {
             0.0
@@ -241,7 +241,7 @@ impl SoundChannel for SoundChannel2 {
             self.wave_duty_position += 1;
             self.wave_duty_position %= 8;
         } else {
-            self.frequency_timer = self.frame_sequence_timer.wrapping_sub(1);
+            self.frequency_timer = self.frequency_timer.wrapping_sub(1);
         }
 
         self.frame_sequence_timer = self.frame_sequence_timer.wrapping_sub(1);
@@ -255,10 +255,10 @@ impl SoundChannel for SoundChannel2 {
                 if self.length == 0 { self.enabled = false; }
             }
 
-            if self.frame_sequence == 7 && self.envelope_enabled && (nr22 & 7) != 0 {
+            if self.frame_sequence == 7 && self.envelope_enabled && (nr22 & 0b111) != 0 {
                 self.envelope_sweeps = self.envelope_sweeps.wrapping_sub(1);
                 if self.envelope_sweeps == 0 {
-                    self.envelope_sweeps = nr22 & 7;
+                    self.envelope_sweeps = nr22 & 0b111;
                     let new_amplitude = self.amplitude + if bit_logic::check_bit(nr22, 3) { 1 } else { -1 };
                     if new_amplitude >= 0 && new_amplitude <= 15 {
                         self.amplitude = new_amplitude;
@@ -271,10 +271,13 @@ impl SoundChannel for SoundChannel2 {
     }
 
     fn get_amplitude(&mut self, memory: &Memory) -> f32 {
-        let nr52: u8 = memory.read_from_memory(0xff26);
-        if self.enabled/* && bit_logic::check_bit(nr52, 2)*/ {
+        if self.enabled {
             let duty = memory.read_from_memory(0xff16) >> 6;
-            if WAVE_FORM[duty as usize][self.wave_duty_position as usize] == 1 { (self.amplitude as f32) / 100.0 } else { 0.0 }
+            if WAVE_FORM[duty as usize][self.wave_duty_position as usize] == 1 {
+                (self.amplitude as f32) / 100.0
+            } else {
+                0.0
+            }
         } else {
             0.0
         }
@@ -282,32 +285,186 @@ impl SoundChannel for SoundChannel2 {
 }
 
 pub(crate) struct SoundChannel3 {
+    enabled: bool,
+    frequency_timer: u16,
     frame_sequence: u8,
     frame_sequence_timer: u16,
+    wave_index: u8,
+    length: u16,
 }
 
 impl SoundChannel3 {
     fn new() -> SoundChannel3 {
         SoundChannel3 {
+            enabled: false,
+            frequency_timer: 0,
             frame_sequence: 8,
             frame_sequence_timer: 8192,
+            wave_index: 0,
+            length: 0,
         }
     }
 }
-impl SoundChannel for SoundChannel3 { }
+
+impl SoundChannel for SoundChannel3 {
+    fn reset(&mut self, memory: &Memory, length: u8) {
+        let nr33 = memory.read_from_memory(0xff1d);
+        let nr34 = memory.read_from_memory(0xff1e);
+        let new_frequency = ((nr34 as u16) & 0b111) << 8 | (nr33 as u16);
+        self.frequency_timer = (2048 - new_frequency) * 2;
+        if self.length == 0 { self.length = 256 - (length as u16); }
+        self.enabled = true;
+        self.wave_index = 0;
+        if memory.read_from_memory(0xff1a) >> 6 == 0 { self.enabled = false; }
+    }
+
+    fn update(&mut self, memory: &mut Memory) {
+        let nr33 = memory.read_from_memory(0xff1d);
+        let nr34 = memory.read_from_memory(0xff1e);
+        if self.frequency_timer == 0 {
+            let new_frequency_timer = (((nr34 as u16) & 0b111) << 8) | (nr33 as u16);
+            self.frequency_timer = (2048 - new_frequency_timer) * 2;
+            self.wave_index += 1;
+            self.wave_index %= 32;
+        } else {
+            self.frequency_timer = self.frequency_timer.wrapping_sub(1);
+        }
+
+        self.frame_sequence_timer = self.frame_sequence_timer.wrapping_sub(1);
+        if self.frame_sequence_timer == 0 {
+            self.frame_sequence_timer = 8192;
+            self.frame_sequence += 1;
+            self.frame_sequence &= 8;
+
+            if self.frame_sequence % 2 == 0 && bit_logic::check_bit(nr34, 6)  && self.length != 0 {
+                self.length = self.length.wrapping_sub(1);
+                if self.length == 0 { self.enabled = false; }
+            }
+        }
+    }
+
+    fn get_amplitude(&mut self, memory: &Memory) -> f32 {
+        if self.enabled {
+            let mut wave = memory.read_from_memory(0xff30 + ((self.wave_index as u16) / 2));
+            wave = if self.wave_index % 2 != 0 {
+                wave & 0xf
+            } else {
+                wave >> 4
+            };
+            let volume = (memory.read_from_memory(0xff1c) >> 5) & 0b11;
+            wave = if volume != 0 {
+                wave >> (volume - 1)
+            } else {
+                wave >> 4
+            };
+            if memory.read_from_memory(0xff1a) >> 7 != 0 {
+                (wave as f32) / 100.0
+            } else {
+                0.0
+            }
+        } else {
+            0.0
+        }
+    }
+}
 
 pub(crate) struct SoundChannel4 {
+    enabled: bool,
+    amplitude: i16,
+    frequency_timer: u16,
     frame_sequence: u8,
     frame_sequence_timer: u16,
+    envelope_enabled: bool,
+    envelope_sweeps: u8,
+    length: u8,
+    lfsr: u16,
 }
 
 impl SoundChannel4 {
     fn new() -> SoundChannel4 {
         SoundChannel4 {
+            enabled: false,
+            amplitude: 0,
+            frequency_timer: 0,
             frame_sequence: 7,
             frame_sequence_timer: 8192,
+            envelope_enabled: false,
+            envelope_sweeps: 0,
+            length: 0,
+            lfsr: 0,
         }
     }
 }
 
-impl SoundChannel for SoundChannel4 { }
+impl SoundChannel for SoundChannel4 {
+    fn reset(&mut self, memory: &Memory, length: u8) {
+        let nr42 = memory.read_from_memory(0xff21);
+        let nr43 = memory.read_from_memory(0xff22);
+        if self.length == 0 { self.length = 64 - length; }
+        self.enabled = true;
+        self.frequency_timer = (SOUND_CHANNEL_4_DIVISOR[(nr43 as usize) & 0b111] as u16) << ((nr43 as u16) >> 4);
+        self.lfsr = 0x7fff;
+        self.amplitude = nr42 as i16 >> 4;
+        self.envelope_sweeps = nr42 & 0b111;
+        self.envelope_enabled = true;
+        if nr42 >> 3 == 0 { self.enabled = false; }
+    }
+
+    fn update(&mut self, memory: &mut Memory) {
+        let nr42 = memory.read_from_memory(0xff21);
+        let nr43 = memory.read_from_memory(0xff22);
+        let nr44 = memory.read_from_memory(0xff23);
+
+        self.frame_sequence_timer = self.frame_sequence_timer.wrapping_sub(1);
+        if self.frame_sequence_timer == 0 {
+            self.frame_sequence_timer = 8192;
+            self.frame_sequence += 1;
+            self.frame_sequence &= 8;
+
+            if self.frame_sequence % 2 == 0 && bit_logic::check_bit(nr44, 6)  && self.length != 0 {
+                self.length = self.length.wrapping_sub(1);
+                if self.length == 0 { self.enabled = false; }
+            }
+
+            if self.frame_sequence == 7 && self.envelope_enabled && (nr42 & 0b111) != 0 {
+                self.envelope_sweeps = self.envelope_sweeps.wrapping_sub(1);
+                if self.envelope_sweeps == 0 {
+                    self.envelope_sweeps = nr42 & 0b111;
+                    if self.envelope_sweeps != 0 {
+                        let new_amplitude = self.amplitude + if bit_logic::check_bit(nr42, 3) { 1 } else { -1 };
+                        if new_amplitude >= 0 && new_amplitude <= 15 {
+                            self.amplitude = new_amplitude;
+                        } else {
+                            self.envelope_enabled = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        if self.frequency_timer == 0 {
+            self.frequency_timer = (SOUND_CHANNEL_4_DIVISOR[(nr43 as usize) & 0b111] as u16) << ((nr43 as u16) >> 4);
+            let xor_rs = (self.lfsr & 1) ^ ((self.lfsr & 0b10) >> 1);
+            self.lfsr >>= 1;
+            self.lfsr |= xor_rs << 14;
+            if bit_logic::check_bit(nr43, 3) {
+                self.lfsr |= xor_rs << 6;
+                self.lfsr &= 0x7f;
+            }
+        } else {
+            self.frequency_timer = self.frequency_timer.wrapping_sub(1);
+        }
+    }
+
+    fn get_amplitude(&mut self, _memory: &Memory) -> f32 {
+        if self.enabled {
+            if (self.lfsr & 1) != 0 {
+                0.0
+            } else {
+                (self.amplitude as f32) / 100.0
+            }
+        } else {
+            0.0
+        }
+    }
+}
